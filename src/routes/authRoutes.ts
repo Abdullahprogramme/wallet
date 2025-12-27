15410205dd96bb2a82352b7aa1ed39ec09829f59
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-12-27 14:13:56 +0500

    Add update password route to authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, updatePassword  } from "../controllers/authController";
+import { auth } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.put("/update-password/:userId", auth, updatePassword);
 
 export default router;
